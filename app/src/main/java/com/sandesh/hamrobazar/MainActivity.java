aeb62add288ea0ef46ae394a42aeae6e94d50189
sandeshkc <sandeshkc999@gmail.com>
2020-01-02 20:13:41 +0545

    Initial commit

---
+++ app/src/main/java/com/sandesh/hamrobazar/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sandesh.hamrobazar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //change
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
